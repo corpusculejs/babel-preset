a75756c5df9c74f6e423a4636b3c7576b0377755
Vlad Rindevich <rindevich.vs@gmail.com>
2019-03-26 01:53:37 +0300

    refactor: use lib directory instead of src

--- __tests__/fixtures/injectors-prop-name/options.js
+++ __tests__/fixtures/injectors-prop-name/options.js
@@ -4,7 +4,7 @@ const cwd = process.cwd();
 module.exports = {
   plugins: [
     [
-      require(resolve(cwd, 'src/babel-plugin-inject-decorator-initializer')),
+      require(resolve(cwd, 'lib/babel-plugin-inject-decorator-initializer')),
       {injectorsPropName: '__customInjectors'},
     ],
     [require('@babel/plugin-proposal-decorators'), {legacy: true}],
--- __tests__/fixtures/simple-with-constructor/options.js
+++ __tests__/fixtures/simple-with-constructor/options.js
@@ -3,7 +3,7 @@ const cwd = process.cwd();
 
 module.exports = {
   plugins: [
-    require(resolve(cwd, 'src/babel-plugin-inject-decorator-initializer')),
+    require(resolve(cwd, 'lib/babel-plugin-inject-decorator-initializer')),
     [require('@babel/plugin-proposal-decorators'), {legacy: true}],
   ],
 };
