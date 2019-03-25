73422fe4e03b4b31424a333a210b56966d8c2c49
Vlad Rindevich <rindevich.vs@gmail.com>
2019-03-26 01:58:48 +0300

    test: remove unnecessary grouping

--- __tests__/index.js
+++ __tests__/index.js
@@ -11,42 +11,40 @@ const execute = async fixture => {
 };
 
 describe('babel-plugin-inject-decorator-initializer', () => {
-  describe('different classes', () => {
-    it('injects function into a class without constructor', async () => {
-      await execute('simple-class');
-    });
+  it('injects function into a class without constructor', async () => {
+    await execute('simple-class');
+  });
 
-    it('injects function into an extended class without constructor', async () => {
-      await execute('extended-class');
-    });
+  it('injects function into an extended class without constructor', async () => {
+    await execute('extended-class');
+  });
 
-    it('injects function into a class with constructor', async () => {
-      await execute('simple-with-constructor');
-    });
+  it('injects function into a class with constructor', async () => {
+    await execute('simple-with-constructor');
+  });
 
-    it('injects function into an extended class with constructor', async () => {
-      await execute('extended-with-constructor');
-    });
+  it('injects function into an extended class with constructor', async () => {
+    await execute('extended-with-constructor');
+  });
 
-    it('does not break the order of constructor statements', async () => {
-      await execute('extended-with-call-before-super');
-    });
+  it('does not break the order of constructor statements', async () => {
+    await execute('extended-with-call-before-super');
+  });
 
-    it('does not break other class memebers', async () => {
-      await execute('keep-class-members');
-    });
+  it('does not break other class memebers', async () => {
+    await execute('keep-class-members');
+  });
 
-    it('works with multiple classes', async () => {
-      await execute('multiple-classes');
-    });
+  it('works with multiple classes', async () => {
+    await execute('multiple-classes');
+  });
 
-    it('runs both extending class and super class injections', async () => {
-      await execute('super-injections');
-    });
+  it('runs both extending class and super class injections', async () => {
+    await execute('super-injections');
+  });
 
-    it('works with property decorators', async () => {
-      await execute('property-decorators');
-    });
+  it('works with property decorators', async () => {
+    await execute('property-decorators');
   });
 
   it('runs injections before user-defined constructor', async () => {
